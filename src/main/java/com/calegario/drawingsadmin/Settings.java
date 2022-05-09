bf514dec05f8dea32487a1716fa7d467d1f3487a
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-05-09 14:48:51 -0300

    recompiled

--- src/main/java/com/calegario/drawingsadmin/Settings.java
+++ src/main/java/com/calegario/drawingsadmin/Settings.java
@@ -1,6 +1,6 @@
 package com.calegario.drawingsadmin;
 
 public class Settings {
-    public static String CSV_PATH = "";
-    public static String DRAWINGS_DIR = "";
+    public static String CSV_PATH = "C:/Users/lucas.correa/Desktop/test.csv";
+    public static String DRAWINGS_DIR = "//clbrfs/Operational/GERENCIAMENTO DE PROJETOS";
 }
