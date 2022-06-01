baf904851e245eb18270f1cc5a16af1124d40ee0
Lucas Calegario <74337880+xlurio@users.noreply.github.com>
2022-06-01 10:33:56 -0300

    Update Settings.java

--- src/main/java/com/calegario/drawingsadmin/Settings.java
+++ src/main/java/com/calegario/drawingsadmin/Settings.java
@@ -4,5 +4,5 @@ import java.io.File;
 
 public class Settings {
     public static String CSV_PATH = (new File("resources/drawingspath.csv")).getAbsolutePath();
-    public static String DRAWINGS_DIR = "//clbrfs/Operational/GERENCIAMENTO DE PROJETOS";
+    public static String DRAWINGS_DIR = "";
 }
